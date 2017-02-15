6983ae57ebfdf4b62f463fe6be52564a1a077a70
sinhajyoti <sinhajyoti@hotmail.com>
2017-02-15 18:40:09 +0530

    Initial commit

---
+++ MyStatelessService1/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace MyStatelessService1
+{
+    public static class Startup
+    {
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
